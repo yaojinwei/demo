cb16a26ab63fbc482e4f248b6018e297ff6275e7
yaojinwei <yjw0909@163.com>
2016-09-24 23:41:37 +0800

    工厂方法多是使用动态类装入器一起组合使用

--- design-patterns-study/design-factory/src/main/java/method/FactoryMethod.java
+++ design-patterns-study/design-factory/src/main/java/method/FactoryMethod.java
@@ -7,6 +7,8 @@ package method;
  * 1、构造函数需要做大量的初始化工作
  * 2、对以后增加的子类可以方便的扩展
  * 3、增加子类不需要修改代码
+ *
+ * 在实际应用中，工厂方法用得比较多一些，而且是和动态类装入器组合在一起应用
  * Created by yaojinwei on 2016/9/24.
  */
 public interface FactoryMethod {
