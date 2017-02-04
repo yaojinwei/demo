e631246a57675ef70c02ea8b1649baef2a84e6e6
yaojinwei <yaojinwei@le.com>
2017-02-04 20:25:30 +0800

    增加maven-test

---
+++ spring-boot-demo/spring-boot-webapp/src/main/java/com/yaojinwei/demo/springboot/config/WebMvcConfigure.java
@@ -0,0 +1,38 @@
+package com.yaojinwei.demo.springboot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.File;
+
+@Configuration
+public class WebMvcConfigure extends WebMvcConfigurerAdapter {
+
+    @Value("${tomcat.server.document-root}")
+    String documentRoot;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.jsp");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//
+//        if (documentRoot != null && documentRoot.length() > 0) {
+//            tomcat.setDocumentRoot(new File(documentRoot));
+//        }
+//
+//        return tomcat;
+//    }
+}
